ed2ae4dbd549598456125edb6788244b5ebe2ac8
Mahmoud Abdo <mahmoud.abdo59986@gmail.com>
2020-06-22 14:16:08 +0200

    Updates

---
+++ precache-manifest.ed1bad79e94f3f384e10279629cc9cb0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cae97c2ed2fdca274d048a4854ea646",
+    "url": "/Natours/index.html"
+  },
+  {
+    "revision": "ce013bebc95d5ae20d3b",
+    "url": "/Natours/static/css/main.18d4c95b.chunk.css"
+  },
+  {
+    "revision": "bfcd505ea4309485f8a7",
+    "url": "/Natours/static/js/2.ce3083af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Natours/static/js/2.ce3083af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce013bebc95d5ae20d3b",
+    "url": "/Natours/static/js/main.9ae9f721.chunk.js"
+  },
+  {
+    "revision": "13f2254bad1cf8b6cd76",
+    "url": "/Natours/static/js/runtime-main.3a990f8a.js"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/Natours/static/media/hero.6e09f410.jpg"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/Natours/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/Natours/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/Natours/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/Natours/static/media/linea-basic-10.e6763f00.ttf"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/Natours/static/media/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/Natours/static/media/nat-4.61bbf12a.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/Natours/static/media/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/Natours/static/media/nat-6.1ef6d764.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/Natours/static/media/nat-7.5f846d16.jpg"
+  }
+]);
